1c1faccdeb004767aaab1d8a6d39d8b583731ad9
Teldoo <43885428+teldoogroup@users.noreply.github.com>
2022-11-02 12:39:55 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+
+    // Application Name
+    'appName' : 'Teldoo POS',
+
+    // Hide elements by ID
+    'hideElementsId' : [],
+
+    // Hide elements by Class
+    'hideElementsClass' : [],
+
+}
+
+module.exports = config
